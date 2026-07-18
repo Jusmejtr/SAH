a44cad3fbda4f7cef9a752d5a050934a2ee89bf7
Jusmejtr <jusmejtr@gmail.com>
2026-07-18 18:10:24 +0200

    init

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,11 @@ import { Button } from "@mui/material";
 import { render } from "preact";
 
 export function App() {
-  return <Button variant="contained">Hello world</Button>;
+  return (
+    <Button variant="contained" onClick={() => window.location.href = "https://github.com/Jusmejtr/SAH/"}>
+      Hello world
+    </Button>
+  );
 }
 
 render(<App />, document.getElementById("app")!);
